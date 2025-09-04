da7c36a38478a380e6e7acce9af75facdf20c406
Student <Student@WIN-30R6SD751UQ>
2025-09-04 14:39:33 +0500

    Создание модели User-а (первичная версия) и миграций. Настройка строки подключения к базе данных.

--- AC_Entities/ACDbContext.cs
+++ AC_Entities/ACDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AC_Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AC_Entities
 {
     public class ACDbContext : DbContext
     {
         public ACDbContext(DbContextOptions<ACDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
     }
 }
