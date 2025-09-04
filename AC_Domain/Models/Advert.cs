2d339641dfd0b17045a6e4c2dd4205da568bb392
Student <Student@WIN-30R6SD751UQ>
2025-09-04 15:02:32 +0500

    Добавление модели "Advert", отвечающей за объявления на сайте.

---
+++ AC_Domain/Models/Advert.cs
@@ -0,0 +1,13 @@
+﻿namespace AC_Domain.Models
+{
+    public class Advert
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } // Заголовок объявления
+        public string Description { get; set; } // Основной текст объявления
+
+        public string Email { get; set; } // Электронная почта для связи
+        public string PhoneNumber { get; set; } // Номер телефона для связи
+    }
+}
--- AC_Entities/Migrations/20250904100100_AddAdvert.Designer.cs
+++ AC_Entities/Migrations/20250904100100_AddAdvert.Designer.cs
@@ -3,6 +3,7 @@ using AC_Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AC_Entities.Migrations
 {
     [DbContext(typeof(ACDbContext))]
-    partial class ACDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250904100100_AddAdvert")]
+    partial class AddAdvert
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
