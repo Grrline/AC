ba66975af3b6a37e9125a084debc2727dc4030fa
Kenny919-wg <ar1ur14250@gmail.com>
2025-09-11 16:16:40 +0500

    Пересборка проекта.

--- AC_Entities/ACDbContext.cs
+++ AC_Entities/ACDbContext.cs
@@ -1,14 +1,9 @@
-﻿using AC_Domain.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace AC_Entities
 {
     public class ACDbContext : DbContext
     {
         public ACDbContext(DbContextOptions<ACDbContext> options) : base(options) { }
-
-        public DbSet<User> Users { get; set; }
-        public DbSet<Advert> Adverts { get; set; }
-        public DbSet<Resume> Resumes { get; set; }
     }
 }
