f2bf704c3f48fe58836fc7e3c14676650a6e9532
Kenny919-wg <ar1ur14250@gmail.com>
2025-09-12 13:28:13 +0500

    Пересборка пересборки.

---
+++ AC_Domain/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace AC_Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        // Не уверен
+        public string PhoneNumber { get; set; }
+        public string PfpPath { get; set; } // путь к profile picture пользователя
+    }
+}
--- AC_Entities/ACDbContext.cs
+++ AC_Entities/ACDbContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AC_Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AC_Entities
 {
     public class ACDbContext : DbContext
     {
         public ACDbContext(DbContextOptions<ACDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Advert> Adverts { get; set; }
+        public DbSet<Resume> Resumes { get; set; }
     }
 }
