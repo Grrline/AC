f722eb2f0809499618d3b30e475343e332cba133
Kenny919-wg <ar1ur14250@gmail.com>
2025-09-12 13:51:08 +0500

    Создание контроллеров и представлений для Users, Resumes и Adverts.

--- AC_Entities/ACDbContext.cs
+++ AC_Entities/ACDbContext.cs
@@ -6,6 +6,10 @@ namespace AC_Entities
     public class ACDbContext : DbContext
     {
         public ACDbContext(DbContextOptions<ACDbContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Advert> Adverts { get; set; }
